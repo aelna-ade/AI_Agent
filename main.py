9fef80d46ca54352dd3b807df2fa66e1746f780c
AbdounB <abdoun.banou@hotmail.fr>
2026-03-15 13:50:56 +0100

    update

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
 
 
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    user_prompt = args.user_prompt
+    print(f"User prompt: {user_prompt}")
+    print("--------------------------------")
     response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.5-flash", contents=messages
     )
     if response.usage_metadata is None:
         raise RuntimeError("Failed API request")
